2ac4a717a0ae81f44b1db68a953ae98b9a581ff5
BranchDev <67700890+branchdev98@users.noreply.github.com>
2022-01-16 06:25:55 +0900

    first

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello CodeSandbox Awesome!</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h2>Yes! This is incredible thing! </h2>
     </div>
   );
 }
